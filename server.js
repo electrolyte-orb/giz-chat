c185535eba094758a6e00238b8e34b12eeb5b2fd
electrolyte-orb <55880176+electrolyte-orb@users.noreply.github.com>
2020-09-20 12:57:08 +0530

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
+});
+
+let users = []
+
+io.on("connection", (socket) => {
+	socket.on("newUsr", data => {
+		users.push(data)
+	})
+	socket.on("chat-msg", (data) => {
+		socket.broadcast.emit("chat-msg", data)
+    })
+})
+
+http.listen((process.env.PORT || 8000), () => {
+	console.log(`Your server is started on port 8000`);
+});
